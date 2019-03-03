dc80a5b09c367ecaaf847f8d869dbfff3645b5f5
John Lee <drumwolf@Johns-MacBook-Pro.local>
2019-03-03 09:13:08 -0800

    refactored table into ul-list format, style just for Macbook screen for now

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,57 +1,47 @@
 import React, { Component } from 'react';
 
-const CalendarHead = () => {
+const CalendarHeader = () => {
   return (
-    <thead>
-      <tr>
-        <th>Sunday</th>
-        <th>Monday</th>
-        <th>Tuesday</th>
-        <th>Wednesday</th>
-        <th>Thursday</th>
-        <th>Friday</th>
-        <th>Saturday</th>
-      </tr>
-    </thead>
+    <header className="CalendarHeader">
+      <h1 className="CalendarHeading">May / June 2019</h1>
+    </header>
   );
 }
 
-const CalendarCaption = () => {
+const CalendarWeek = () => {
   return (
-    <caption>
-      <h1>May / June 2019</h1>
-    </caption>
+    <ul className="CalendarWeek">
+      <li>Sunday</li>
+      <li>Monday</li>
+      <li>Tuesday</li>
+      <li>Wednesday</li>
+      <li>Thursday</li>
+      <li>Friday</li>
+      <li>Saturday</li>
+    </ul>
   );
 }
 
 const CalendarBody = () => {
+  let calendarItems = [];
+  for (let i = 1; i <= 28; i++) {
+    calendarItems.push(<li>{i}</li>);
+  }
   return (
-    <tbody>
-      <tr>
-        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
-      </tr>
-      <tr>
-        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
-      </tr>
-      <tr>
-        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
-      </tr>
-      <tr>
-        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
-      </tr>
-    </tbody>
-  )
+    <ul className="CalendarBody">{calendarItems}</ul>
+  );
 }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <table className="Calendar">
-          <CalendarCaption />
-          <CalendarHead />
+        <main className="Calendar">
+          <CalendarHeader />
+          <CalendarWeek />
           <CalendarBody />
-        </table>
+        </main>
+        <aside className="Sidebar"></aside>
       </div>
     );
   }

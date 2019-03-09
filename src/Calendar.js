43e7681e491e9636e8a70b433012983960c16544
John Lee <drumwolf@Johns-MacBook-Pro.local>
2019-03-09 12:29:07 -0800

    Refactor CalendarComponents into own JS / pass schedule data down to CalendarComponents

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,58 +1,29 @@
 import React, { Component } from 'react';
 import CalendarData from './CalendarData.js';
-
-const CalendarHeader = (props) => {
-  return (
-    <header className="CalendarHeader">
-      <h1 className="CalendarHeading">{props.title}</h1>
-    </header>
-  );
-}
-
-const CalendarWeek = () => {
-  return (
-    <ul className="CalendarWeek">
-      <li>Sunday</li>
-      <li>Monday</li>
-      <li>Tuesday</li>
-      <li>Wednesday</li>
-      <li>Thursday</li>
-      <li>Friday</li>
-      <li>Saturday</li>
-    </ul>
-  );
-}
-
-const CalendarBody = (props) => {
-  const calendarDates = props.dates;
-  const calendarItems = [];
-  const firstMonth = Number( calendarDates[0].split('-')[1] );
-  for (let i = 0; i < 28; i++) {
-    const dateString = calendarDates[i];
-    const dateNumber = Number( dateString.split('-')[2] );
-    const dateMonth  = Number( dateString.split('-')[1] );
-    const monthClass = (firstMonth === dateMonth) ? 'first-month' : 'second-month';
-    const todayClass = (props.today === dateString) ? 'today' : '';
-    const classNames = [monthClass, todayClass].join(' ');
-    calendarItems.push(<li key={dateString} id={dateString} className={classNames}>{ dateNumber }</li>);
-  }
-  return (
-    <ul className="CalendarBody">{calendarItems}</ul>
-  );
-}
+import { CalendarHeader, CalendarWeek, CalendarBody } from './CalendarComponents.js';
 
 class Calendar extends Component {
   constructor(props) {
     super(props);
-    const cd = new CalendarData(props.date);
-    this.state = cd.data;
+    this.state = new CalendarData(props.date).data;
+  }
+  formatScheduleData(data) {
+    let date = '', hash = {};
+    for (let i = 0; i < data.length; i++) {
+          const obj = data[i];
+          date = (obj.date) ? obj.date : date;
+          hash[date] = hash[date] || [];
+          hash[date].push(obj.plans);
+    }
+    return hash;
   }
   render() {
+    const schedule = this.formatScheduleData(this.props.schedule)
     return (
       <main className="Calendar">
         <CalendarHeader title={this.state.months} />
         <CalendarWeek />
-        <CalendarBody dates={this.state.dates} today={this.state.today} />
+        <CalendarBody dates={this.state.dates} today={this.state.today} schedule={schedule} />
       </main>
     );
   }
---
+++ src/CalendarComponents.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const CalendarHeader = (props) => {
+  return (
+    <header className="CalendarHeader">
+      <h1 className="CalendarHeading">{props.title}</h1>
+    </header>
+  );
+}
+
+const CalendarWeek = () => {
+  return (
+    <ul className="CalendarWeek">
+      <li>Sunday</li>
+      <li>Monday</li>
+      <li>Tuesday</li>
+      <li>Wednesday</li>
+      <li>Thursday</li>
+      <li>Friday</li>
+      <li>Saturday</li>
+    </ul>
+  );
+}
+
+const CalendarBody = (props) => {
+  console.log('props',props.schedule)
+  const calendarDates = props.dates;
+  const calendarItems = [];
+  const firstMonth = Number( calendarDates[0].split('-')[1] );
+  for (let i = 0; i < 28; i++) {
+    const dateString = calendarDates[i];
+    const dateNumber = Number( dateString.split('-')[2] );
+    const dateMonth  = Number( dateString.split('-')[1] );
+    const monthClass = (firstMonth === dateMonth) ? 'first-month' : 'second-month';
+    const todayClass = (props.today === dateString) ? 'today' : '';
+    const classNames = [monthClass, todayClass].join(' ');
+    const schedule = ( props.schedule[dateString] ) ? props.schedule[dateString].map( obj => <p>{obj}</p>) : "";
+    calendarItems.push(
+      <li key={dateString} id={dateString} className={classNames}>
+        <div className="dateNumber">{ dateNumber }</div>
+        <div className="dateSchedule">{ schedule }</div>
+      </li>
+    );
+  }
+  return (
+    <ul className="CalendarBody">{calendarItems}</ul>
+  );
+}
+
+export { CalendarHeader, CalendarWeek, CalendarBody }

a6120e45ceee38ef1a3a7d3d4df68be4434340f2
John Lee <drumwolf@Johns-MacBook-Pro.local>
2019-03-10 08:32:28 -0700

    enable sidebar to slide in and out

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Sidebar = (props) => {
   const schedule = (props.schedule) ? props.schedule.map( item => <li>{item}</li> ) : null;
+  const status = (props.schedule) ? 'open' : null;
+  const styles = ['Sidebar', status].join(' ');
   return (
-    <aside className="Sidebar">
+    <aside className={styles}>
       <h2>{props.date}</h2>
       <ul>{schedule}</ul>
     </aside>

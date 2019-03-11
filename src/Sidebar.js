aab16f50fd3a46a9fae07e770f7268a825ab79b0
John Lee <drumwolfmini@Johns-Mac-mini.local>
2019-03-10 17:54:57 -0700

    formatted day/date header in sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+const DAYSPERWEEK = [null, 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 const Sidebar = (props) => {
-  const schedule = (props.schedule) ? props.schedule.map( item => <li>{item}</li> ) : null;
+  const schedule = (props.schedule) ? props.schedule.map( item => <li key={item}>{item}</li> ) : null;
   const status = (props.schedule) ? 'open' : null;
   const styles = ['Sidebar', status].join(' ');
+  const formatDate = (dateString) => {
+    if (!dateString) { return null }
+    const [yy, mm, dd] = dateString.split('-').map( obj => Number(obj) );
+    const date = new Date(yy, mm - 1, dd);
+    const day = DAYSPERWEEK[ date.getDay() ];
+    return `${day} ${mm}/${dd}`;
+  }
   return (
     <aside className={styles} onClick={props.onClick}>
-      <h2>{props.date}</h2>
+      <h2><code>{formatDate(props.date)}</code></h2>
       <ul>{schedule}</ul>
     </aside>
   );

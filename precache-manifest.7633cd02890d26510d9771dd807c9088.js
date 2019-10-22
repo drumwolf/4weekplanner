0f6e94ca472405a84d5b794b823107c0671ddd2d
John Lee <drumwolf@gmail.com>
2019-10-22 15:37:16 -0700

    Updates

---
+++ precache-manifest.7633cd02890d26510d9771dd807c9088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ab5a9fedc357458e6e8",
+    "url": "/4weekplanner/static/css/main.67dfe907.chunk.css"
+  },
+  {
+    "revision": "8ab5a9fedc357458e6e8",
+    "url": "/4weekplanner/static/js/main.8ab5a9fe.chunk.js"
+  },
+  {
+    "revision": "b21066432e44fedf2f46",
+    "url": "/4weekplanner/static/js/runtime~main.b2106643.js"
+  },
+  {
+    "revision": "a124ee1cc41fb75e6b55",
+    "url": "/4weekplanner/static/js/2.a124ee1c.chunk.js"
+  },
+  {
+    "revision": "aeac12495b4e630b728e3d2aadf1ad5c",
+    "url": "/4weekplanner/index.html"
+  }
+];
